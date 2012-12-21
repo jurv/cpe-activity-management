be83d296df5c7e114f40d7e46d9a5a3b32afdec9
jul.rouvier@gmail.com <jul.rouvier@gmail.com@30ed3e7d-4013-1159-5544-c3fd33945701>
2012-12-21 13:55:44 +0000

    Remplissage de JPABase

---
+++ trunk/JPABase/src/model/Customer.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the Customer database table.
+ * 
+ */
+@Entity
+public class Customer implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="cus_id")
+	private int cusId;
+
+	@Column(name="cus_isdeleted")
+	private byte cusIsdeleted;
+
+	@Column(name="cus_name")
+	private String cusName;
+
+	public Customer() {
+	}
+
+	public int getCusId() {
+		return this.cusId;
+	}
+
+	public void setCusId(int cusId) {
+		this.cusId = cusId;
+	}
+
+	public byte getCusIsdeleted() {
+		return this.cusIsdeleted;
+	}
+
+	public void setCusIsdeleted(byte cusIsdeleted) {
+		this.cusIsdeleted = cusIsdeleted;
+	}
+
+	public String getCusName() {
+		return this.cusName;
+	}
+
+	public void setCusName(String cusName) {
+		this.cusName = cusName;
+	}
+
+}
