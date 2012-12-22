482f6e49cc4e7834754dbd39a7d2bb52914d9e68
benjamin.chastanier@gmail.com <benjamin.chastanier@gmail.com@30ed3e7d-4013-1159-5544-c3fd33945701>
2012-12-22 13:33:41 +0000

    Ajout de classes form tables

---
+++ trunk/EJB/ejbModule/com/cpeeterprise/BeanUser.java
@@ -0,0 +1,61 @@
+package com.cpeeterprise;
+
+import java.util.List;
+
+import javax.ejb.Stateful;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import model.User;
+
+/**
+ * Session Bean implementation class BeanUser
+ */
+@Stateful(mappedName = "User")
+@TransactionManagement(TransactionManagementType.BEAN)
+public class BeanUser implements BeanUserRemote {
+	
+	 @PersistenceContext
+	 private EntityManager em;
+	 
+    /**
+     * Default constructor. 
+     */
+    public BeanUser() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void persist (User user) {
+        em.persist (user);
+      }
+
+      public void delete (User user) {
+        User t = em.merge (user);
+        em.remove( t );
+
+      }
+
+      public void update (User user) {
+        em.merge (user);
+      }
+
+      public List <User> findUsers () {
+
+        return (List <User>) em.createQuery("select t from User t").getResultList();
+      }
+
+      public User findUser (String id) {
+
+        return (User) em.find(User.class, Long.parseLong(id));
+      }
+
+	@Override
+	public boolean connectUser(User user) {
+		
+		return false;
+	}
+
+
+}
