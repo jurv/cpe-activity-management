2e0cc10c24e88f369e1c46ca25ff48343a8a3f54
benjamin.chastanier@gmail.com <benjamin.chastanier@gmail.com@30ed3e7d-4013-1159-5544-c3fd33945701>
2013-01-03 16:18:58 +0000

    Debut interface chat

--- WEB/src/managedbeans/ConnectedUsersBean.java
+++ WEB/src/managedbeans/ConnectedUsersBean.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.component.html.HtmlPanelGroup;
+import javax.faces.event.ActionEvent;
 
 import model.User;
 
@@ -21,6 +23,7 @@ public class ConnectedUsersBean {
 	private User currentUser;
 	private List<User> connectedUsers;
 	private String connectedUsersString = "";
+	private String connectedUsersWindowsString = "";
 
 	public User getCurrentUser() {
 		return currentUser;
@@ -43,7 +46,7 @@ public class ConnectedUsersBean {
 		setConnectedUsers(userRemote.getConnectedUsers());
 		String connectedUsersString = "";
 		for( User cur : connectedUsers ) {
-			connectedUsersString += "<div class='chat-element' pid='" + cur.getUsrId() + "'>" + cur.getUsrFirstname() + " " + cur.getUsrLastname() + "</div>" + "<br/>";
+			connectedUsersString += "<div class='chat-element' pid='" + cur.getUsrId() + "' pfname='" + cur.getUsrFirstname() + "' plname='" + cur.getUsrLastname() + "' >" + cur.getUsrFirstname() + " " + cur.getUsrLastname() + "</div>" + "<br/>";
 		}
 		
 		return connectedUsersString;
@@ -51,6 +54,37 @@ public class ConnectedUsersBean {
 
 	public void setConnectedUsersString(String connectedUsersString) {
 		this.connectedUsersString = connectedUsersString;
+	}
+	
+	public void sendMessage (ActionEvent evt) {
+
+		String test = "";
+	}
+
+	public String getConnectedUsersWindowsString() {
+		
+		String connectedUsersWindowsString = "";
+		for( User cur : connectedUsers ) {
+			
+			connectedUsersWindowsString += "<div class='chat-window' pid='" + cur.getUsrId() + "'>" +
+										   "	<div class='chat-window-header'> " + cur.getUsrFirstname() + " " + cur.getUsrLastname() + 
+										   "	</div>" +
+										   "	<div id='message-content-" + cur.getUsrId() + "' class='chat-window-content'>" +
+										   "    	<div class='chat-window-content-controls'> " +
+										   "			<h:form id='form-" + cur.getUsrId() + "' name='form-" + cur.getUsrId() + "'> " +	
+		   					   			   "	        	<h:outputText id='messages-" + cur.getUsrId() + "'> </h:outputText> " +
+		   					   			   "				<h:commandButton value='Go' actionListener='#{ConnectedUsersBean.sendMessage}'> </h:commandButton> " +
+		   					   			   "                <h:inputText id='input-text-" + cur.getUsrId() + "'></h:outputText> " +
+		   					   			   "            </h:form> " +
+										   "        </div>" +
+										   "    </div>" +
+										   "</div>";
+		}
+		return connectedUsersWindowsString;
+	}
+
+	public void setConnectedUsersWindowsString(String connectedUsersWindowsString) {
+		this.connectedUsersWindowsString = connectedUsersWindowsString;
 	}
 
 }
