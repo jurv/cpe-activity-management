603c003fb9e858c8c73b931fba8cc080b0bd009d
vincent.mullier@gmail.com <vincent.mullier@gmail.com@30ed3e7d-4013-1159-5544-c3fd33945701>
2013-01-12 17:13:22 +0000

    Ajout de la vue liste des clients. + Modification de outputbean

--- trunk/WEB/src/managedbeans/OutputBean.java
+++ trunk/WEB/src/managedbeans/OutputBean.java
@@ -7,25 +7,41 @@ import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 
+import model.Customer;
 import model.Project;
 
 
+import com.cpeeterprise.BeanCustomerRemote;
 import com.cpeeterprise.BeanProjectRemote;
 
-@ManagedBean(name="project")
+@ManagedBean(name="output")
 @SessionScoped
 public class OutputBean {
 	
 	@EJB
 	public BeanProjectRemote projectRemote;
+	
+	@EJB
+	public BeanCustomerRemote customerRemote;
+	
+	
 	public Project test;
 	
 	private List<Project> refProjects;
+	private List<Customer> refCustomers;
+	
 
 	public List<Project> getProjectList() {
 		if (refProjects == null) {
 			refProjects = projectRemote.findProjects();
 		}
 		return refProjects;
+	}
+	
+	public List<Customer> getCustomerList() {
+		if (refCustomers == null) {
+			refCustomers = customerRemote.findCustomers();
+		}
+		return refCustomers;
 	}
 }
