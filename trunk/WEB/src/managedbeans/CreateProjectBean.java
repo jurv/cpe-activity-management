54a98655089a8d993634a9e3ebe7486654578275
jul.rouvier@gmail.com <jul.rouvier@gmail.com@30ed3e7d-4013-1159-5544-c3fd33945701>
2013-01-13 17:15:10 +0000

    Page de création de projet fonctionnelle

--- trunk/WEB/src/managedbeans/CreateProjectBean.java
+++ trunk/WEB/src/managedbeans/CreateProjectBean.java
@@ -5,8 +5,11 @@ import java.util.Date;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import model.Project;
 import com.cpeeterprise.BeanProjectRemote;
+import com.cpeeterprise.BeanUser2ProjectRemote;
+
+import model.Project;
+import model.User2Project;
 
 @ManagedBean
 @SessionScoped
@@ -14,21 +17,49 @@ public class CreateProjectBean {
 	
 	@EJB
 	public BeanProjectRemote projectRemote;
+	@EJB
+	public BeanUser2ProjectRemote user2projectRemote;
+	
 	private String projectName = "";
 	private String projectTotalTime = "";
 	private String projectComment = "";
 	private Date projectDateCreated = new Date();
 	private Date projectDateFinished = new Date();
+	private int projectCusId;
+	private int projectCdpId;
 	
 	public void updateProject()
 	{
 		Project project = new Project();
+		User2Project user2project = new User2Project();
+		
 		project.setPrjLabel(this.projectName);
 		project.setPrjDateCreated(this.projectDateCreated);
 		project.setPrjDateFinished(this.projectDateFinished);
 		project.setPrjComment(this.projectComment);
-		project.setCusId(1);
-		projectRemote.update(project);
+		project.setCusId(this.projectCusId);
+		project = projectRemote.persist(project);
+		
+		user2project.setPrjId(project.getPrjId());
+		user2project.setFctId(2);
+		user2project.setUsrId(this.projectCdpId);
+		user2projectRemote.persist(user2project);
+	}
+
+	public int getProjectCusId() {
+		return this.projectCusId;
+	}
+
+	public void setProjectCusId(int c) {
+		this.projectCusId = c;
+	}
+
+	public int getProjectCdpId() {
+		return projectCdpId;
+	}
+
+	public void setProjectCdpId(int projectCdpId) {
+		this.projectCdpId = projectCdpId;
 	}
 
 	public String getProjectComment() {
