f51e1cc3ff34b024721b5ee9df90f7164da95c92
benjamin.chastanier@gmail.com <benjamin.chastanier@gmail.com@30ed3e7d-4013-1159-5544-c3fd33945701>
2013-01-14 15:13:28 +0000

    Suite historique chat

---
+++ trunk/WEB/src/managedbeans/ChatsProjectBean.java
@@ -0,0 +1,63 @@
+package managedbeans;
+
+
+import java.util.ArrayList;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import model.Message;
+import model.Project;
+import model.User;
+
+
+import com.cpeeterprise.BeanMessageRemote;
+import com.cpeeterprise.BeanProjectRemote;
+import com.cpeeterprise.BeanUserRemote;
+
+@ManagedBean
+@SessionScoped
+public class ChatsProjectBean {
+	
+	@EJB
+	public BeanUserRemote userRemote;
+	@EJB
+	public BeanProjectRemote projectRemote;
+	
+	private User currentUser = new User();	
+	private ArrayList <Project> managedProject = new ArrayList<Project>();
+	private ArrayList <Project> ownProject = new ArrayList<Project>();
+
+	public void initView() {
+		// Récupération de la liste des projets de l'utilisateur
+		this.ownProject.clear();
+		this.ownProject.addAll(projectRemote.findProjectsByUser(this.currentUser.getUsrId()));
+	}
+	
+	public User getCurrentUser() {
+		return currentUser;
+	}
+
+	public void setCurrentUser(User currentUser) {
+		this.currentUser = currentUser;
+	}
+
+	public ArrayList <Project> getManagedProject() {
+		return managedProject;
+	}
+
+	public void setManagedProject(ArrayList <Project> managedProject) {
+		this.managedProject = managedProject;
+	}
+
+	public ArrayList <Project> getOwnProject() {
+		return ownProject;
+	}
+
+	public void setOwnProject(ArrayList <Project> ownProject) {
+		this.ownProject = ownProject;
+	}
+}
