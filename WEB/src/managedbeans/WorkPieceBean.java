b6ebb625b303ebd1dfde25e1e0fdf748a6c7331f
jul.rouvier@gmail.com <jul.rouvier@gmail.com@30ed3e7d-4013-1159-5544-c3fd33945701>
2013-01-15 10:37:57 +0000

    Ajout de la saisie des temps passés sur les taches

---
+++ WEB/src/managedbeans/WorkPieceBean.java
@@ -0,0 +1,127 @@
+package managedbeans;
+
+
+import java.util.Date;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import model.Task;
+import model.User;
+import model.WorkPiece;
+
+import com.cpeeterprise.BeanTaskRemote;
+import com.cpeeterprise.BeanWorkPiece;
+import com.cpeeterprise.BeanWorkPieceRemote;
+
+@ManagedBean
+@SessionScoped
+public class WorkPieceBean {
+	
+	@EJB
+	public BeanWorkPieceRemote workPieceRemote;
+	
+	private String workPieceComment = "";
+	private int workPieceTskId = 0;
+	private int workPieceUsrId = 0;
+	private Date workPieceDate = new Date();
+	private int workPieceDuration = 0;
+	
+	private User currentUser = new User();	
+	
+	
+	public BeanWorkPieceRemote getWorkPieceRemote() {
+		return workPieceRemote;
+	}
+
+
+
+	public void setWorkPieceRemote(BeanWorkPieceRemote workPieceRemote) {
+		this.workPieceRemote = workPieceRemote;
+	}
+
+
+
+	public String getWorkPieceComment() {
+		return workPieceComment;
+	}
+
+
+
+	public void setWorkPieceComment(String workPieceComment) {
+		this.workPieceComment = workPieceComment;
+	}
+
+
+
+	public int getWorkPieceTskId() {
+		return workPieceTskId;
+	}
+
+
+
+	public void setWorkPieceTskId(int workPieceTskId) {
+		this.workPieceTskId = workPieceTskId;
+	}
+
+
+
+	public int getWorkPieceUsrId() {
+		return workPieceUsrId;
+	}
+
+
+
+	public void setWorkPieceUsrId(int workPieceUsrId) {
+		this.workPieceUsrId = workPieceUsrId;
+	}
+
+
+
+	public Date getWorkPieceDate() {
+		return workPieceDate;
+	}
+
+
+
+	public void setWorkPieceDate(Date workPieceDate) {
+		this.workPieceDate = workPieceDate;
+	}
+
+
+
+	public int getWorkPieceDuration() {
+		return workPieceDuration;
+	}
+
+
+
+	public void setWorkPieceDuration(int workPieceDuration) {
+		this.workPieceDuration = workPieceDuration;
+	}
+
+
+
+	public void createWorkPiece()
+	{
+		
+		WorkPiece workPiece = new WorkPiece();
+		workPiece.setTskId(this.workPieceTskId);
+		workPiece.setUsrId(this.workPieceUsrId);
+		workPiece.setWrkComment(this.workPieceComment);
+		workPiece.setWrkDate(this.workPieceDate);
+		workPiece.setWrkDuration(this.workPieceDuration);
+			
+		workPieceRemote.persist(workPiece);
+	}	
+		
+	public User getCurrentUser() {
+		return currentUser;
+	}
+
+	public void setCurrentUser(User currentUser) {
+		this.currentUser = currentUser;
+	}
+}
