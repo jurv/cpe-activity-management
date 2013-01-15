a823e6613b5592946322bbf2d40663670fde0393
benjamin.chastanier@gmail.com <benjamin.chastanier@gmail.com@30ed3e7d-4013-1159-5544-c3fd33945701>
2013-01-15 08:39:53 +0000

    Ajout détails chat

---
+++ WEB/src/managedbeans/ViewChatBean.java
@@ -0,0 +1,62 @@
+package managedbeans;
+
+
+import java.util.ArrayList;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import model.Message;
+import model.User;
+
+import com.cpeeterprise.BeanMessageRemote;
+import com.cpeeterprise.BeanProjectRemote;
+import com.cpeeterprise.BeanUserRemote;
+
+@ManagedBean
+@ViewScoped
+public class ViewChatBean {
+	
+	@EJB
+	public BeanUserRemote userRemote;
+	@EJB
+	public BeanMessageRemote messageRemote;
+	@EJB
+	public BeanProjectRemote projectRemote;
+	
+	private User currentUser;
+	private User currentUserChatter;
+	private ArrayList<Message> messagesList = new ArrayList<Message>();
+	
+	
+	public void initPage() {
+
+		// Récupération de l'utilisateur communiquant
+		String param = (String)FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("usrChatterId") ;
+		Integer usrChatterId = (param != null && param != "") ? Integer.parseInt(param) : 0;  
+		if(usrChatterId > 0)
+			this.currentUserChatter = userRemote.findUser(usrChatterId);
+	
+		// Récupération des messages
+		this.messagesList.clear();
+		this.messagesList.addAll(messageRemote.findMessagesForConv(this.currentUser.getUsrId(), this.currentUserChatter.getUsrId()));
+	}
+
+	public User getCurrentUser() {
+		return currentUser;
+	}
+
+	public void setCurrentUser(User currentUser) {
+		this.currentUser = currentUser;
+	}
+
+	public User getCurrentUserChatter() {
+		return currentUserChatter;
+	}
+
+	public void setCurrentUserChatter(User currentUserChatter) {
+		this.currentUserChatter = currentUserChatter;
+	}
+}
