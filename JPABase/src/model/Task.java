b90079a36ef98d1cc9660e6d30888eda6505fd67
jul.rouvier@gmail.com <jul.rouvier@gmail.com@30ed3e7d-4013-1159-5544-c3fd33945701>
2013-01-16 21:07:36 +0000

    Restoration du mode de génération auto de l'ID

--- JPABase/src/model/Task.java
+++ JPABase/src/model/Task.java
@@ -14,6 +14,7 @@ public class Task implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name="tsk_id")
 	private int tskId;
 
