38a03ff4f0352ccf55da6f43fbbb01a83f8d9a75
benjamin.chastanier@gmail.com <benjamin.chastanier@gmail.com@30ed3e7d-4013-1159-5544-c3fd33945701>
2013-01-16 19:19:42 +0000

    Debut d'ajout des stats sur le dashboard

--- WEB/src/managedbeans/DashboardBean.java
+++ WEB/src/managedbeans/DashboardBean.java
@@ -1,6 +1,8 @@
 package managedbeans;
 
 
+import java.util.HashMap;
+
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -19,6 +21,14 @@ public class DashboardBean {
 	@EJB
 	public BeanUserRemote userRemote;
 	private User currentUser;
+	private HashMap <String, Integer> tasksStatus = new HashMap<String, Integer>(); 
+	
+	public void initView () {
+		
+		// On remplit les décomptes de tâches pour l'utilisateur courant
+		
+		
+	}
 
 	public User getCurrentUser() {
 		return currentUser;
@@ -26,5 +36,13 @@ public class DashboardBean {
 
 	public void setCurrentUser(User currentUser) {
 		this.currentUser = currentUser;
+	}
+
+	public HashMap <String, Integer> getTasksStatus() {
+		return tasksStatus;
+	}
+
+	public void setTasksStatus(HashMap <String, Integer> tasksStatus) {
+		this.tasksStatus = tasksStatus;
 	}
 }
